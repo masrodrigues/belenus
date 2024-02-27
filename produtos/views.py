0e4d93770335c11db3a91c008c88606d20ac5e57
Marco Rodrigues <masrodrigues@inf.ufrgs.br>
2024-02-27 10:09:36 -0300

    Ajuste Busca

--- produtos/views.py
+++ produtos/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import Produto
 
+from django.db.models import Q
+
 def pesquisa_produto(request):
-    query = request.GET.get('q', '')  # Obtém o parâmetro de pesquisa da URL
-    produtos = Produto.objects.all()
-    if query:  # Se o parâmetro 'q' não estiver vazio, filtra os produtos
-        produtos = produtos.filter(codigo_do_produto__icontains=query) | produtos.filter(descricao__icontains=query)
+    query = request.GET.get('q', '')
+
+    # Divide a string de consulta em palavras
+    query_words = query.split()
+
+    # Cria uma query que procura cada palavra em qualquer parte dos campos desejados
+    search_query = Q()
+    for word in query_words:
+        search_query &= (
+            Q(descricao__icontains=word) | 
+            Q(codigo_do_produto__icontains=word)
+        )
+    
+    # Filtra os produtos com a query construída
+    produtos = Produto.objects.filter(search_query)
+
     return render(request, 'produtos/pesquisa.html', {'produtos': produtos, 'query': query})
+
