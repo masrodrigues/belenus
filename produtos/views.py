8f52cd012b06786a9aae96f3587250b3a3cb896c
Marco Rodrigues <masrodrigues@inf.ufrgs.br>
2024-02-27 20:26:27 -0300

    nova atualização

--- produtos/views.py
+++ produtos/views.py
@@ -1,24 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Produto
-
 from django.db.models import Q
 
 def pesquisa_produto(request):
     query = request.GET.get('q', '')
-
-    # Divide a string de consulta em palavras
     query_words = query.split()
-
-    # Cria uma query que procura cada palavra em qualquer parte dos campos desejados
     search_query = Q()
-    for word in query_words:
-        search_query &= (
-            Q(descricao__icontains=word) | 
-            Q(codigo_do_produto__icontains=word)
-        )
-    
-    # Filtra os produtos com a query construída
-    produtos = Produto.objects.filter(search_query)
 
+    for word in query_words:
+        search_query &= (Q(descricao__icontains=word) | Q(codigo_do_produto__icontains=word))
+    
+    produtos = Produto.objects.filter(search_query)
     return render(request, 'produtos/pesquisa.html', {'produtos': produtos, 'query': query})
 
+def produto_update(request):
+    if request.method == 'POST':
+        produto_id = request.POST.get('produto_id')
+        codigo = request.POST.get('codigo')
+        descricao = request.POST.get('descricao')
+        venda = request.POST.get('venda', None)
+        revenda = request.POST.get('revenda', None)
+        atacado = request.POST.get('atacado', None)
+
+        if not produto_id:
+            messages.error(request, 'ID do produto não fornecido.')
+            return redirect('pesquisa_produto') # Corrigido para usar o nome da URL
+        
+        produto = get_object_or_404(Produto, pk=produto_id)
+        produto.codigo_do_produto = codigo
+        produto.descricao = descricao
+
+        # Atualiza os campos se forem fornecidos
+        if venda is not None:
+            produto.venda = venda
+        if revenda is not None:
+            produto.revenda = revenda
+        if atacado is not None:
+            produto.atacado = atacado
+
+        produto.save()
+
+        messages.success(request, 'Produto atualizado com sucesso.')
+        return redirect('pesquisa_produto') # Corrigido para usar o nome da URL
